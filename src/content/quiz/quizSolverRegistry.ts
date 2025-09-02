5c9d05dc03432484aea7e5fc86a6536dfa6ba694
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 01:01:28 +0200

    feat: add registry for quiz solvers

---
+++ src/content/quiz/quizSolverRegistry.ts
@@ -0,0 +1,11 @@
+import { DocumentFacade } from "../DocumentFacade";
+import { PageType } from "../jetpunk/PageType";
+import { QuizSolver } from "./solvers/QuizSolver";
+
+export type Constructor<T extends QuizSolver = QuizSolver> = new (documentFacade: DocumentFacade) => T;
+
+export const registry = new Map<PageType, Constructor>();
+
+export function register(name: PageType, ctor: Constructor) {
+    registry.set(name, ctor);
+}
