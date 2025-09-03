bab4b755907c23013a3ca5789c4c2e483d6b5030
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:38:54 +0200

    refactor: apply prettier to PictureQuizSolver.ts

--- src/content/quiz/solvers/PictureQuizSolver.ts
+++ src/content/quiz/solvers/PictureQuizSolver.ts
@@ -1,8 +1,7 @@
-import { PageType } from "../../jetpunk/PageType";
-import { register } from "../quizSolverRegistry";
-import { TypeInQuizSolver } from "./TypeInQuizSolver";
+import { PageType } from '../../jetpunk/PageType';
+import { register } from '../quizSolverRegistry';
+import { TypeInQuizSolver } from './TypeInQuizSolver';
 
-export class PictureQuizSolver extends TypeInQuizSolver {
-}
+export class PictureQuizSolver extends TypeInQuizSolver {}
 
 register(PageType.PHOTO_GAME, PictureQuizSolver);
