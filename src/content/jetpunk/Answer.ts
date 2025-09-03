22110ee91f2061c90911077aaa20b48e0a8bafaf
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 19:40:51 +0200

    feat: add answers to PageVar

---
+++ src/content/jetpunk/Answer.ts
@@ -0,0 +1,8 @@
+export default interface Answer {
+    id: string;
+    display: string;
+    isName: boolean;
+    node: string;
+    cols: string[];
+    path: string;
+};
--- src/content/jetpunk/PageVar.ts
+++ src/content/jetpunk/PageVar.ts
@@ -1,8 +1,10 @@
+import Answer from "./Answer";
 import { PageType } from "./PageType";
 
 export default interface PageVar {
     data: {
         quiz: {
+            answers: Answer[],
         },
     },
     pageType: PageType,
