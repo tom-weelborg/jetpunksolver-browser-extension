ef7ea77f7a6e2ecc042f9bf4fd19f7efe00ac7b3
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:37:39 +0200

    refactor: apply prettier to quizSolverFactory.ts

--- src/content/quiz/quizSolverFactory.ts
+++ src/content/quiz/quizSolverFactory.ts
@@ -1,23 +1,23 @@
-import { registry } from "./quizSolverRegistry";
-import * as QuizSolvers from "./solvers/nonAbstractQuizSolvers";
-import { DocumentFacade } from "../DocumentFacade";
-import { QuizSolver } from "./solvers/QuizSolver";
+import { registry } from './quizSolverRegistry';
+import * as QuizSolvers from './solvers/nonAbstractQuizSolvers';
+import { DocumentFacade } from '../DocumentFacade';
+import { QuizSolver } from './solvers/QuizSolver';
 
 export function createSolver(documentFacade: DocumentFacade): QuizSolver {
-    loadSolvers();
+	loadSolvers();
 
-    const pageVar = documentFacade.getPageVar();
-    const pageType = pageVar.pageType;
+	const pageVar = documentFacade.getPageVar();
+	const pageType = pageVar.pageType;
 
-    for (const [name, Ctor] of registry) {
-        if (name === pageType) {
-            return new Ctor(documentFacade);
-        }
-    }
-    
-    throw new Error('No solver for given page type found');
+	for (const [name, Ctor] of registry) {
+		if (name === pageType) {
+			return new Ctor(documentFacade);
+		}
+	}
+
+	throw new Error('No solver for given page type found');
 }
 
 function loadSolvers(): void {
-    console.log('The following quiz solver classes are loaded:', QuizSolvers);
+	console.log('The following quiz solver classes are loaded:', QuizSolvers);
 }
