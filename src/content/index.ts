c2c36eb9d12b1f40c8e37dc42076c5001c654714
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:35:05 +0200

    refactor: apply prettier to index.ts

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,7 +1,7 @@
-import { createAutoSolveButton } from "./autoSolveButton";
-import { DocumentFacade } from "./DocumentFacade";
-import JetPunkConfig from "./jetpunk/JetPunkConfig";
-import { createSolver } from "./quiz/quizSolverFactory";
+import { createAutoSolveButton } from './autoSolveButton';
+import { DocumentFacade } from './DocumentFacade';
+import JetPunkConfig from './jetpunk/JetPunkConfig';
+import { createSolver } from './quiz/quizSolverFactory';
 
 createAutoSolveButton().addEventListener('click', function () {
 	const documentFacade = new DocumentFacade(document);
