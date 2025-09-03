031716018cae7a3c1569c8abccaaacdaa87f2b39
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:40:41 +0200

    refactor: apply prettier to Answer.ts

--- src/content/jetpunk/Answer.ts
+++ src/content/jetpunk/Answer.ts
@@ -1,8 +1,8 @@
 export default interface Answer {
-    id: string;
-    display: string;
-    isName: boolean;
-    node: string;
-    cols: string[];
-    path: string;
-};
+	id: string;
+	display: string;
+	isName: boolean;
+	node: string;
+	cols: string[];
+	path: string;
+}
