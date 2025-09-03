2bb99503bd2e3eb4f85c5c57e2389b89a1e88bab
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 19:39:27 +0200

    feat: add QuizSolver class structure

--- src/content/quiz/solvers/QuizSolver.ts
+++ src/content/quiz/solvers/QuizSolver.ts
@@ -2,4 +2,34 @@ import { DocumentFacade } from "../../DocumentFacade";
 
 export abstract class QuizSolver {
     constructor(protected readonly documentFacade: DocumentFacade) {}
+
+    public solve(): boolean {
+        let solved = true;
+        const questions = this.getQuestions();
+        for (const question of questions) {
+            if (!this.solveQuestion(question)) {
+                solved = false;
+            }
+        }
+        return solved;
+    }
+
+    protected abstract getQuestions(): string[];
+
+    protected solveQuestion(question: string): boolean {
+        const answers = this.getAnswers(question);
+        for (const answer of answers) {
+            this.enterAnswer(answer);
+            if (this.isQuestionSolved(question, answers)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected abstract getAnswers(question: string): string[];
+
+    protected abstract enterAnswer(answer: string): void;
+
+    protected abstract isQuestionSolved(question: string, answers: string[]): boolean;
 }
