db82aa67554fa27b1d67650d2ff8d93854f77027
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 20:47:44 +0200

    feat: add TextQuizSolver

---
+++ src/content/quiz/solvers/TextQuizSolver.ts
@@ -0,0 +1,8 @@
+import { PageType } from "../../jetpunk/PageType";
+import { register } from "../quizSolverRegistry";
+import { TypeInQuizSolver } from "./TypeInQuizSolver";
+
+export class TextQuizSolver extends TypeInQuizSolver {
+}
+
+register(PageType.TEXT_GAME, TextQuizSolver);
