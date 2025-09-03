9aeea7b9d1a952b112273986a6e768cca1d38ff5
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:38:25 +0200

    refactor: apply prettier to TypeInQuizSolver.ts

--- src/content/quiz/solvers/TypeInQuizSolver.ts
+++ src/content/quiz/solvers/TypeInQuizSolver.ts
@@ -1,24 +1,26 @@
-import JetPunkConfig from "../../jetpunk/JetPunkConfig";
-import { QuizSolver } from "./QuizSolver";
+import JetPunkConfig from '../../jetpunk/JetPunkConfig';
+import { QuizSolver } from './QuizSolver';
 
 export abstract class TypeInQuizSolver extends QuizSolver {
-    protected getQuestions(): string[] {
-        return this.documentFacade.getPageVar().data.quiz.answers
-            .map(answer => answer.id);
-    }
+	protected getQuestions(): string[] {
+		return this.documentFacade.getPageVar().data.quiz.answers.map((answer) => answer.id);
+	}
 
-    protected getAnswers(question: string): string[] {
-        return this.documentFacade.getPageVar().data.quiz.answers
-            .filter(answer => answer.id === question)
-            .map(answer => answer.display);
-    }
+	protected getAnswers(question: string): string[] {
+		return this.documentFacade
+			.getPageVar()
+			.data.quiz.answers.filter((answer) => answer.id === question)
+			.map((answer) => answer.display);
+	}
 
-    protected enterAnswer(answer: string): void {
-        this.documentFacade.typeToElement(JetPunkConfig.textAnswerInputBoxSelector, answer);
-        this.documentFacade.clearElement(JetPunkConfig.textAnswerInputBoxSelector);
-    }
+	protected enterAnswer(answer: string): void {
+		this.documentFacade.typeToElement(JetPunkConfig.textAnswerInputBoxSelector, answer);
+		this.documentFacade.clearElement(JetPunkConfig.textAnswerInputBoxSelector);
+	}
 
-    protected isQuestionSolved(question: string, answers: string[]): boolean {
-        return this.documentFacade.doesElementExist(`[${JetPunkConfig.answerCssProperty}="${question}"]${JetPunkConfig.correctAnswerSelector}`);
-    }
+	protected isQuestionSolved(question: string, answers: string[]): boolean {
+		return this.documentFacade.doesElementExist(
+			`[${JetPunkConfig.answerCssProperty}="${question}"]${JetPunkConfig.correctAnswerSelector}`
+		);
+	}
 }
