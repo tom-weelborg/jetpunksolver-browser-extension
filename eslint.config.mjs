f219b3d31faa78401b868941a43ef6b0a86d7530
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 23:10:54 +0200

    refactor: reorder imports in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import globals from 'globals';
 import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 import js from '@eslint/js';
 import nounsanitized from 'eslint-plugin-no-unsanitized';
 import tseslint from 'typescript-eslint';
