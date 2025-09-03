a796865caf15387b88c821585185cc097e991f6f
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:39:11 +0200

    refactor: apply prettier to QuizSolver.ts

--- src/content/quiz/solvers/QuizSolver.ts
+++ src/content/quiz/solvers/QuizSolver.ts
@@ -1,35 +1,35 @@
-import { DocumentFacade } from "../../DocumentFacade";
+import { DocumentFacade } from '../../DocumentFacade';
 
 export abstract class QuizSolver {
-    constructor(protected readonly documentFacade: DocumentFacade) {}
+	constructor(protected readonly documentFacade: DocumentFacade) {}
 
-    public solve(): boolean {
-        let solved = true;
-        const questions = this.getQuestions();
-        for (const question of questions) {
-            if (!this.solveQuestion(question)) {
-                solved = false;
-            }
-        }
-        return solved;
-    }
+	public solve(): boolean {
+		let solved = true;
+		const questions = this.getQuestions();
+		for (const question of questions) {
+			if (!this.solveQuestion(question)) {
+				solved = false;
+			}
+		}
+		return solved;
+	}
 
-    protected abstract getQuestions(): string[];
+	protected abstract getQuestions(): string[];
 
-    protected solveQuestion(question: string): boolean {
-        const answers = this.getAnswers(question);
-        for (const answer of answers) {
-            this.enterAnswer(answer);
-            if (this.isQuestionSolved(question, answers)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	protected solveQuestion(question: string): boolean {
+		const answers = this.getAnswers(question);
+		for (const answer of answers) {
+			this.enterAnswer(answer);
+			if (this.isQuestionSolved(question, answers)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    protected abstract getAnswers(question: string): string[];
+	protected abstract getAnswers(question: string): string[];
 
-    protected abstract enterAnswer(answer: string): void;
+	protected abstract enterAnswer(answer: string): void;
 
-    protected abstract isQuestionSolved(question: string, answers: string[]): boolean;
+	protected abstract isQuestionSolved(question: string, answers: string[]): boolean;
 }
