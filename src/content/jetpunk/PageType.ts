60c795657722e7400e5230d5e484798bec48a9c8
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:40:16 +0200

    refactor: apply prettier to PageType.ts

--- src/content/jetpunk/PageType.ts
+++ src/content/jetpunk/PageType.ts
@@ -1,12 +1,12 @@
 export const PageType = {
-    CLICK_GAME: 'click-game',
-    DAILY_GAME: 'daily-game',
-    MAP_GAME: 'map-game',
-    MULTIPLE_CHOICE_GAME: 'mc-game',
-    PHOTO_GAME: 'photo-game',
-    SUDDEN_DEATH_GAME: 'sd-game',
-    TEXT_GAME: 'text-game',
-    TILE_GAME: 'tile-game',
+	CLICK_GAME: 'click-game',
+	DAILY_GAME: 'daily-game',
+	MAP_GAME: 'map-game',
+	MULTIPLE_CHOICE_GAME: 'mc-game',
+	PHOTO_GAME: 'photo-game',
+	SUDDEN_DEATH_GAME: 'sd-game',
+	TEXT_GAME: 'text-game',
+	TILE_GAME: 'tile-game'
 } as const;
 
 export type PageType = (typeof PageType)[keyof typeof PageType];
