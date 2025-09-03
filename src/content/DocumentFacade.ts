a45b3d0119618395f3843724751b9de7c42e4873
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:36:33 +0200

    refactor: apply prettier to DocumentFacade.ts

--- src/content/DocumentFacade.ts
+++ src/content/DocumentFacade.ts
@@ -1,73 +1,73 @@
-import PageVar from "./jetpunk/PageVar";
+import PageVar from './jetpunk/PageVar';
 
 export class DocumentFacade {
-    private pageVar: PageVar | undefined;
+	private pageVar: PageVar | undefined;
 
-    constructor(private readonly document: Document) {}
+	constructor(private readonly document: Document) {}
 
-    public clearElement(querySelector: string): void {
-        this.typeToElement(querySelector, '');
-    }
+	public clearElement(querySelector: string): void {
+		this.typeToElement(querySelector, '');
+	}
 
-    public clickElement(querySelector: string): void {
-        const element = this.document.querySelector(querySelector);
-        if (element instanceof HTMLElement) {
-            element.click();
-        }
-    }
+	public clickElement(querySelector: string): void {
+		const element = this.document.querySelector(querySelector);
+		if (element instanceof HTMLElement) {
+			element.click();
+		}
+	}
 
-    public doesElementExist(querySelector: string): boolean {
-        return this.document.querySelector(querySelector) !== null;
-    }
+	public doesElementExist(querySelector: string): boolean {
+		return this.document.querySelector(querySelector) !== null;
+	}
 
-    public getPageVar(): PageVar {
-        this.pageVar ??= this.findPageVar();
+	public getPageVar(): PageVar {
+		this.pageVar ??= this.findPageVar();
 
-        return this.pageVar;
-    }
+		return this.pageVar;
+	}
 
-    private findPageVar(): PageVar {
-        const scriptTags = this.document.getElementsByTagName('script');
-        const pattern = /_page = (.*);/;
+	private findPageVar(): PageVar {
+		const scriptTags = this.document.getElementsByTagName('script');
+		const pattern = /_page = (.*);/;
 
-        for (const scriptTag of scriptTags) {
-            const scriptTagAsString = scriptTag.innerHTML;
-            const matches = scriptTagAsString.match(pattern);
-            if (matches && matches.length > 1) {
-                const pageVarString = matches[1];
-                const pageVarObject = JSON.parse(pageVarString);
-                return pageVarObject as PageVar;
-            }
-        }
+		for (const scriptTag of scriptTags) {
+			const scriptTagAsString = scriptTag.innerHTML;
+			const matches = scriptTagAsString.match(pattern);
+			if (matches && matches.length > 1) {
+				const pageVarString = matches[1];
+				const pageVarObject = JSON.parse(pageVarString);
+				return pageVarObject as PageVar;
+			}
+		}
 
-        throw new Error('Variable "_page" not found in any "script" tag');
-    }
+		throw new Error('Variable "_page" not found in any "script" tag');
+	}
 
-    public typeToElement(querySelector: string, text: string): void {
-        const element = this.document.querySelector(querySelector);
-        if (element instanceof HTMLInputElement) {
-            this.typeStringToElement(element, text);
-        }
-    }
+	public typeToElement(querySelector: string, text: string): void {
+		const element = this.document.querySelector(querySelector);
+		if (element instanceof HTMLInputElement) {
+			this.typeStringToElement(element, text);
+		}
+	}
 
-    private typeStringToElement(element: HTMLInputElement, text: string): void {
-        if (text !== '') {
-            for (let char of text) {
-                this.typeCharToElement(element, char);
-            }
-        } else {
-            element.value = text;
-        }
-        element.dispatchEvent(new Event('change', { bubbles: true }));
-    }
+	private typeStringToElement(element: HTMLInputElement, text: string): void {
+		if (text !== '') {
+			for (let char of text) {
+				this.typeCharToElement(element, char);
+			}
+		} else {
+			element.value = text;
+		}
+		element.dispatchEvent(new Event('change', { bubbles: true }));
+	}
 
-    private typeCharToElement(element: HTMLInputElement, char: string): void {
-        element.dispatchEvent(new KeyboardEvent('keydown', { key: char }));
-        element.dispatchEvent(new KeyboardEvent('keypress', { key: char }));
+	private typeCharToElement(element: HTMLInputElement, char: string): void {
+		element.dispatchEvent(new KeyboardEvent('keydown', { key: char }));
+		element.dispatchEvent(new KeyboardEvent('keypress', { key: char }));
 
-        element.value += char;
+		element.value += char;
 
-        element.dispatchEvent(new Event('input', { bubbles: true }));
-        element.dispatchEvent(new KeyboardEvent('keyup', { key: char }));
-    }
+		element.dispatchEvent(new Event('input', { bubbles: true }));
+		element.dispatchEvent(new KeyboardEvent('keyup', { key: char }));
+	}
 }
