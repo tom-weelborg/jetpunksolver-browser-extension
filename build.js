e023783d10636f6a5d198b6ed158b4c790a4e93d
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-03 21:48:02 +0200

    refactor: apply prettier to build.js

--- build.js
+++ build.js
@@ -1,12 +1,12 @@
 import { build } from 'esbuild';
 
 build({
-    outbase: 'src',
-    entryPoints: ['./**/index.ts'],
-    outdir: 'dist',
-    entryNames: '[dir]/[name]',
-    bundle: true,
-    platform: 'browser',
-    sourcemap: false,
-    minify: true,
+	outbase: 'src',
+	entryPoints: ['./**/index.ts'],
+	outdir: 'dist',
+	entryNames: '[dir]/[name]',
+	bundle: true,
+	platform: 'browser',
+	sourcemap: false,
+	minify: true
 }).catch(() => process.exit(1));
