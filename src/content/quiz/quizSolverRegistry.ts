ca6021525270b694458765220c6ec3d9c2fec9dc
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-06 21:19:01 +0200

    refactor: update types in quizSolverRegistry

--- src/content/quiz/quizSolverRegistry.ts
+++ src/content/quiz/quizSolverRegistry.ts
@@ -1,10 +1,10 @@
 import { DocumentFacade } from '../DocumentFacade';
+import PageVar from '../jetpunk/page-var/PageVar';
 import { PageType } from '../jetpunk/PageType';
 import { QuizSolver } from './solvers/QuizSolver';
 
-export type Constructor<T extends QuizSolver = QuizSolver> = new (
-	documentFacade: DocumentFacade
-) => T;
+export type Constructor<T extends QuizSolver<unknown, PageVar> = QuizSolver<unknown, PageVar>> =
+	new (documentFacade: DocumentFacade<PageVar>) => T;
 
 export const registry = new Map<PageType, Constructor>();
 
